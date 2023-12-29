f56bdc177486c975a7f40328e8b6a2aa06855645
Pinguina-Git <151978028+Pinguina-Git@users.noreply.github.com>
2023-12-29 16:44:22 +1300

    updated City on SearchCity

--- src/index.js
+++ src/index.js
@@ -54,4 +54,4 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-searchCity("Lisbon");
+searchCity("Auckland");
